d8b79dfaf05744c962e46b098eda6751fb1d56a8
wheeze-cmd <louismiguelterrago@gmail.com>
2026-05-24 21:42:29 +0800

    backend using supabase

---
+++ js/appointment.js
@@ -0,0 +1,30 @@
+const supabaseUrl = "https://ejnbylyuzzyrmcndflxk.supabase.co";
+const supabaseKey = "sb_publishable_1yW9v4QxoUpKiZV6-HTFmg_hacS2CzP";
+
+const client = supabase.createClient(supabaseUrl, supabaseKey);
+
+const form = document.getElementById("appointmentForm");
+
+form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const data = {
+        name: document.getElementById("name").value,
+        contact: document.getElementById("contact").value,
+        service: document.getElementById("service").value,
+        preferred_time: document.getElementById("preferred_time").value,
+        note: document.getElementById("note").value
+    };
+
+    const { error } = await client
+        .from("appointments")
+        .insert([data]);
+
+    if (error) {
+        alert("Booking failed 😢");
+        console.log(error);
+    } else {
+        alert("Booked successfully 🎉");
+        form.reset();
+    }
+});
